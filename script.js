6105285d9025be02b937d0d72eaa2da7d3ac5612
Pignataro67 <Pignataro67@gmail.com>
2022-04-19 05:09:35 -0700

    adding JS

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+
+  btn.innerText = sound
 
   btn.addEventListener('click', () => {
     stopSongs()
